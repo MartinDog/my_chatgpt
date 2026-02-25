13f2f2b39e072ce7198b47897e6e023e6c6043b9
martin5 <martin5@pagoda21.com>
2026-02-25 13:49:27 +0900

    모든 캐시에 적용되도록 설정 변경

--- src/main/java/com/mychatgpt/config/CacheConfig.java
+++ src/main/java/com/mychatgpt/config/CacheConfig.java
@@ -2,7 +2,6 @@ package com.mychatgpt.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +13,7 @@ public class CacheConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        CaffeineCacheManager manager = new CaffeineCacheManager("vectorSearch");
+        CaffeineCacheManager manager = new CaffeineCacheManager();
         manager.setCaffeine(Caffeine.newBuilder()
                 .expireAfterWrite(10, TimeUnit.MINUTES)
                 .maximumSize(500));
